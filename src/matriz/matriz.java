ac3e3884f9fb2c3d43835e1f70104ead0b9ed823
andré <andre_f_witt@estudante.sesisenai.org.br>
2023-10-10 16:35:11 -0300

    questao3matrizes
    questão sobre numeros positivos e negativos

--- src/matriz/matriz.java
+++ src/matriz/matriz.java
@@ -1,10 +1,35 @@
 package matriz;
-
+	import java.util.Scanner;
 public class matriz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				
+		int matriz [][] = new int [4][4];
+		
+		Scanner sc = new Scanner (System.in);
 
+		
+		for ( int linha = 0; linha < 4; linha++ ) {
+			for ( int coluna = 0; coluna < 4; coluna++) {
+				System.out.println("Digite o valor: ");
+				matriz [linha][coluna] = sc.nextInt();
+				
+				if ( matriz [linha][coluna] < 0) {
+					matriz[linha][coluna] = 0;
+				}
+
+			}
+			
+			
+				}
+		
+		for ( int linhaFim = 0; linhaFim < 4; linhaFim++ ) {
+			for ( int colunaFim = 0; colunaFim < 4; colunaFim++ ) {
+				System.out.println(matriz [linhaFim][colunaFim]);
+			}
+			
+		}
+		
 	}
 
 }
